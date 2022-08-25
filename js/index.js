a717feb1010e6559dd9d577229829eed7388ba5d
Felipe Lerda <lerdafelipe@gmail.com>
2022-08-24 23:35:47 -0300

    New-website

--- js/index.js
+++
@@ -1,6 +0,0 @@
-function showPopup(){
-    $(".lang").addClass("open");
-}
-function hidePopup(){
-    $(".lang").removeClass("open");
-}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,256 @@
-/****************************************** */
-(()=>{
+const recruiterEngl = {
+					homeText: "Hi! I'm Felipe Lerda. I am a front end developer from Argentina. I am so happy and I hope that you too.",
+ 					homeSecondText: "I'm a person who loves to learn and be self-sufficient. Since I was in high school I have taken courses on different topics to deepen and have a more competitive advantage with others. I'm in the world of IT since I take the Wordpress course in 2019. In 2020 I started with the career of Full Stack development at Coderhouse. I love to learn from other people to know more than them. I am constantly thinking of new projects and I love to be useful to others. I would like get a job in a startup or a company where I can better exploit my skills. If you like what you read, send me a message and tomorrow we could have a meeting.",
+					skillsData: "Some tools",
+					skillsSubtitle: "I usually work with the React stack, but i'm open to others technologies.",
+					portfolioTitle: "Some projects",
+					portfolioSubtitle: "* Click on the image and go to the project",
+					contactData: "Contact me",
+					contactTitle: "Keep in touch",
+					phoneTitle: "Phone",
+					loactionTitle: "Location",
+					polits: "Privacy politicy",
+					styleTitle: "Style switcher",
+					LangTitle: "Language",
+					VisitTitle: "Visitor Type",
+					firtVisitor: "Only A Recruiter",
+					secondVisitor: "An important person",
+					thirdVisitor: "A startup owner",
+					popupTitle: "What type of visitor are you?",
+					firstProject: "This is a website for a company with functionalities to automate company processes. <span>Tools:</span> Firestore, React.js",
+					secondProject: "This was an own startup, basically an ecommerce of supermarket items. Currently not available. <span>Tools:</span> Wordpress.",
+					thirdProject: "These are mini project HTML/CSS/JS vanilla. <span>Tools:</span> mostly HTML, CSS, Javascript",
+					fourthProject: "These are projects developed with me with React.js <span>Tools:</span> React.js, Redux, Node.js and other libraries."
+};
+const recruiter = {
+				homeText: "Hola! Soy Felipe Lerda. Soy un front-end developer de Argentina.",
+				homeSecondText: "Soy una persona que le encanta aprender para poder ser autosuficiente. Desde que estaba en la secundaria hago cursos de distintas asignaturas para mejorar mis habilidades y tener una ventaja competitiva frente al resto. Entré en el mundo IT en 2019 después de hacer un curso de Wordpress y en 2020, durante un emprendimiento (e-commerce) decidí meterme de lleno en este mundo a través de la carrera de desarrollo web full stack en coderhouse. Me encanta aprender de otras personas. Constantemente estoy pensando en nuevos proyectos para emprender. Quiero ser productivo y sentirme útil siempre. Espero conseguir un trabajo en una startup o empresa que explote mi creatividad y mi potencial. Si te gusta mi perfil, envíame un mensaje y organizamos una reunión.",
+				skillsData: "Algunas herramientas",
+				skillsSubtitle: "Generalmente trabajo con el stack de React.js pero estoy abierto a aprender otras tecnologías.",
+				portfolioTitle: "Algunos proyectos",
+				portfolioSubtitle: "* Click en la imagen y podrás ir a ver el proyecto",
+				contactData: "Contacto",
+				contactTitle: "Contactate conmigo",
+				phoneTitle: "Teléfono",
+				loactionTitle: "Ubicación",
+				polits: "Politicas de privacidad",
+				styleTitle: "Estilos del sitio",
+				LangTitle: "Idioma",
+				VisitTitle: "Tipo de usuario",
+				firtVisitor: "Un Simple recruiter",
+				secondVisitor: "Una persona importante",
+				thirdVisitor: "Dueño de una startup",
+				popupTitle: "¿Qué tipo de usuario eres?",
+				firstProject: "Es un sitio web de muestra con funcionalidades para automatizar trabajos de la empresa. <span>Herramientas:</span> Firestore, React.js",
+				secondProject: "Este fue un emprendimiento propio, Era un ecommerce con items de supermercado. No disponible. <span>Herramientas:</span> Wordpress.",
+				thirdProject: "Proyectos Hechos con con HTML/CSS/Js vanilla. <span>Herramientas:</span> mayormente hechos con HTML, CSS, Javascript.",
+				fourthProject: "Proyectos desarrollados por mi con React.js. <span>Herramientas:</span> React.js, Redux, Node.js y otras librerias Javascript."
+};
+const userEngl = {
+			homeText: "Hi! I'm Felipe Lerda. I am a front end developer from Argentina. I am so happy and I hope that you too. I'm 20 years old. In 2019, while I was finishing high school, I took a course on EducacionIt and from that moment I am trying to learn the whole world of web design and development. I made a few websites and in 2020 I decided to start a development career in Coderhouse.I am the perfect person to do your web site, because I search the best for your Project to do it the most professional as possible and I strive in all I do.",
+			homeSecondText: "If you are interested please contact me right now to confirm your Project, because I make a limited number of projects per month.",
+			skillsData: "Some tools",
+			skillsSubtitle: "These are some tools that I usually use, not all.",
+			portfolioTitle: "Some projects",
+			portfolioSubtitle: "* Click on the image and go to the project",
+			contactData: "Contact me",
+			contactTitle: "Keep in touch",
+			phoneTitle: "Phone",
+			loactionTitle: "Location",
+			polits: "Privacy politicy",
+			styleTitle: "Style switcher",
+			LangTitle: "Language",
+			VisitTitle: "Visitor Type",
+			firtVisitor: "Only A Recruiter",
+			secondVisitor: "An important person",
+			thirdVisitor: "A startup owner",
+			popupTitle: "What type of visitor are you?",
+			firstProject: "This is a website for a company with functionalities to automate company processes. <span>Tools:</span> Firestore, React.js",
+			secondProject: "This was an own startup, basically an ecommerce of supermarket items. Currently not available. <span>Tools:</span> Wordpress.",
+			thirdProject: "These are mini project HTML/CSS/JS vanilla. <span>Tools:</span> mostly HTML, CSS, Javascript",
+			fourthProject: "These are projects developed with me with React.js <span>Tools:</span> React.js, Redux, Node.js and other libraries."
+};
+const user = {
+			homeText: "¡Hola! Mi nombre es Felipe Lerda. Soy Desarrollador Web. Tengo 19 años, En 2019, mientras estaba en mi último año del secundario, realicé un curso de EducacionIT y desde ese momento incursiono en el mundo del diseño y desarrollo web. Luego de hacer algunos sitios web, en 2020 decidí meterme de lleno en esta profesión y es por eso que comencé una carrera en Coderhouse. Soy la persona indicada para hacer tu sitio web, ya que además de ser alguien que pone mucho esfuerzo en lo que hace, tomo cada proyecto como propio y busco mejoras constantes para que tu idea sea lo más profesional posible.",
+			homeSecondText: "Mi consejo es que te apresures a contactarme para poder confirmar tu sitio web, ya que tengo un límite de proyectos al mes.",
+			skillsData: "Algunas herramientas",
+			skillsSubtitle: "Generalmente trabajo con el stack de React.js pero estoy abierto a aprender otras tecnologías.",
+			portfolioTitle: "Algunos proyectos",
+			portfolioSubtitle: "* Click en la imagen y podrás ir a ver el proyecto",
+			contactData: "Contacto",
+			contactTitle: "Contactate conmigo",
+			phoneTitle: "Teléfono",
+			loactionTitle: "Ubicación",
+			polits: "Politicas de privacidad",
+			styleTitle: "Estilos del sitio",
+			LangTitle: "Idioma",
+			VisitTitle: "Tipo de usuario",
+			firtVisitor: "Un simple recruiter",
+			secondVisitor: "Una persona importante",
+			popupTitle: "¿Qué tipo de usuario eres?",
+			firstProject: "Es un sitio web de muestra con funcionalidades para automatizar trabajos de la empresa. <span>Herramientas:</span> Firestore, React.js",
+			secondProject: "Este fue un emprendimiento propio, Era un ecommerce con items de supermercado. No disponible. <span>Herramientas:</span> Wordpress.",
+			thirdProject: "Proyectos Hechos con con HTML/CSS/Js vanilla. <span>Herramientas:</span> mayormente hechos con HTML, CSS, Javascript.",
+			fourthProject: "Proyectos desarrollados por mi con React.js. <span>Herramientas:</span> React.js, Redux, Node.js y otras librerias Javascript."
+};
+
+let userType = localStorage.getItem('userType');
+let language = localStorage.getItem('lang');
+
+
+const initialize = (typeUs)=>{
+	document.getElementById('home-text').innerHTML = typeUs.homeText;
+	document.getElementById('home-secondText').innerHTML = typeUs.homeSecondText;
+	document.getElementById('skills-data').value = typeUs.skillsData;
+	document.getElementById('skills-subTitle').innerHTML = typeUs.skillsSubtitle;
+	document.getElementById('portfolio-subtitle').innerHTML = typeUs.portfolioSubtitle;
+	document.getElementById('portfolio-title').innerHTML = typeUs.portfolioTitle;
+	document.getElementById('contact-title').value = typeUs.contactData;
+	document.getElementById('contact-title').innerHTML = typeUs.contactTitle;
+	document.getElementById('phone-title').innerHTML = typeUs.phoneTitle;
+	document.getElementById('location-title').innerHTML = typeUs.loactionTitle;
+	document.getElementById('privacy-policy').innerHTML = typeUs.polits;
+	document.getElementById('styleTitle').innerHTML = typeUs.styleTitle;
+	document.getElementById('lang-title').innerHTML = typeUs.LangTitle;
+	document.getElementById('visitor-title').innerHTML = typeUs.VisitTitle;
+	document.getElementById('first-visitor').innerHTML = typeUs.firtVisitor;
+	document.getElementById('second-visitor').innerHTML = typeUs.secondVisitor;
+	document.getElementById('p-first-project').innerHTML = typeUs.firstProject;
+	document.getElementById('p-second-project').innerHTML = typeUs.secondProject;
+	document.getElementById('p-third-project').innerHTML = typeUs.thirdProject;
+	document.getElementById('p-fourth-project').innerHTML = typeUs.fourthProject;
+}
+
+let initializeApp = ()=>{
+	if(userType !== null){
+		document.getElementById('popup').classList.add('hide')
+		if (userType === "recruiter" && language === 'Español'){ 
+			initialize(recruiter);
+			document.getElementById('lang-esp').classList.add('active');
+			document.getElementById('lang-eng').classList.remove('active');
+			document.getElementById('first-visitor').classList.add('active');
+			document.getElementById('second-visitor').classList.remove('active');
+		}
+		else if (userType === "recruiter" && language === 'English'){ 
+			initialize(recruiterEngl);
+			document.getElementById('lang-eng').classList.add('active');
+			document.getElementById('lang-esp').classList.remove('active');
+			document.getElementById('first-visitor').classList.add('active');
+			document.getElementById('second-visitor').classList.remove('active');
+		}
+		else if (userType === "user" && language === 'Español') {
+			initialize(user);
+			document.getElementById('lang-esp').classList.add('active');
+			document.getElementById('lang-eng').classList.remove('active');
+			document.getElementById('second-visitor').classList.add('active');
+			document.getElementById('first-visitor').classList.remove('active');
+		}
+		else if (userType === "user" && language === 'English') {
+			initialize(userEngl);
+			document.getElementById('lang-eng').classList.add('active');
+			document.getElementById('lang-esp').classList.remove('active');
+			document.getElementById('second-visitor').classList.add('active');
+			document.getElementById('first-visitor').classList.remove('active');
+		}
+		else if (userType === 'recruiter' && language === null) {
+			initialize(recruiterEngl);
+			document.getElementById('lang-eng').classList.add('active');
+			document.getElementById('lang-esp').classList.remove('active');
+			document.getElementById('first-visitor').classList.add('active');
+			document.getElementById('second-visitor').classList.remove('active');
+		}
+		else if (userType === 'user' && language === null) {
+			initialize(userEngl);
+			document.getElementById('lang-eng').classList.add('active');
+			document.getElementById('lang-esp').classList.remove('active');
+			document.getElementById('second-visitor').classList.add('active');
+			document.getElementById('first-visitor').classList.remove('active');
+		}
+	}
+}
+
+
+initializeApp();
+
+
+const setUserFirst = (use)=>{
+	localStorage.setItem('userType', use);
+	document.getElementById('popup').classList.add('hide');
+	userType = localStorage.getItem('userType');
+	language = localStorage.getItem('lang');
+	initializeApp();
+}
+
+const setLangFirst = () =>{
+	language = localStorage.getItem('lang');
+	if(language === "Español" || language === null){ 
+		localStorage.setItem('lang', "English");
+		document.getElementById('switchet-lang').classList.remove('active');
+		document.getElementById('lang-eng').classList.add('active');
+		document.getElementById('lang-esp').classList.remove('active');
+		//**Popup */
+		document.getElementById('title-popup').innerHTML = userEngl.popupTitle;
+		document.getElementById('recr').innerHTML = userEngl.firtVisitor;
+		document.getElementById('user-ty').innerHTML = userEngl.secondVisitor;
+	}
+	if(language === "English"){ 
+		document.getElementById('switchet-lang').classList.add('active');
+		localStorage.setItem('lang', "Español");
+		document.getElementById('lang-esp').classList.remove('active');
+		document.getElementById('lang-eng').classList.add('active');
+		//**Popup */
+		document.getElementById('title-popup').innerHTML = user.popupTitle;
+		document.getElementById('recr').innerHTML = user.firtVisitor;
+		document.getElementById('user-ty').innerHTML = user.secondVisitor;
+	}
+}
+
+
+
+const changeLang = (lang)=>{
+	localStorage.setItem('lang', lang);
+	language = localStorage.getItem('lang');
+	initializeApp();
+
+}
+
+
+const changeUserType = (type)=>{
+	localStorage.setItem('userType', type);
+	userType = localStorage.getItem('userType');
+	initializeApp();
+}
+
+
+
+
+
+
+
+
+
+
+window.addEventListener("load", ()=>{
+	document.querySelector(".preloader").classList.add("fadeOut");
+	setTimeout(()=>{
+		document.querySelector(".preloader").style.display="none";
+	},600)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*************Web anterior funciones**************** */
+/*(()=>{
 	let ubicacion = window.location.hash;
 	const navMenu = document.querySelector(".nav-menu");
 	if(ubicacion !==""){
@@ -56,12 +307,12 @@
 		}
 	}
 })();
-
+*/
 
 
 /**********menu*********************/
 
-(()=>{
+/*(()=>{
 	const hamburgerBtn = document.querySelector(".hamburger-btn"),
 	navMenu = document.querySelector(".nav-menu"),
 	closeNavBtn = navMenu.querySelector(".close-nav-menu");
@@ -101,7 +352,7 @@
 				//
 				navMenu.querySelector(".active").classList.add("outer-shadow", "hover-in-shadow");
 				navMenu.querySelector(".active").classList.remove("active", "inner-shadow");
-				/**************************/
+				
 				if(navMenu.classList.contains("open")){
 				   //
 				   event.target.classList.add("active", "inner-shadow");
@@ -126,37 +377,11 @@
 	})
 
 
-})();
-
-
-/*-----Seccion sobre, pestañas------*/
-
-(()=>{
-	const sobreSection = document.querySelector(".sobre-section"),
-	tabsContainer = document.querySelector(".sobre-pestañas")
-
-	tabsContainer.addEventListener("click", (event) =>{
-		if(event.target.classList.contains("tab-item") && !event.target.classList.contains("active")){
-			const target = event.target.getAttribute("data-target");
-			//
-			tabsContainer.querySelector(".active").classList.remove("outer-shadow", "active");
-			//
-			event.target.classList.add("active", "outer-shadow");
-			//
-			sobreSection.querySelector(".tab-content.active").classList.remove("active");
-			//
-			sobreSection.querySelector(target).classList.add("active");
-		}
-	})
-})();
-
-function bodyScrollingToggle(){
-	document.body.classList.toggle("stop-scrolling");
-}
+})();*/
 
 /*------------------------Popup y filto portfolio------------------------------------------------*/
 
-(() =>{
+/*(() =>{
 	const filterContainer = document.querySelector(".portfolio-filter"),
 	portfolioItemsContainer = document.querySelector(".portfolio-items"),
 	portfolioItems = document.querySelectorAll(".portfolio-item"),
@@ -294,24 +519,4 @@ function bodyScrollingToggle(){
     	}
     }
 
-})();
-
-
-/***************control de secciones*********/
-
-(() =>{
-	const sections = document.querySelectorAll(".section");
-	sections.forEach((section) =>{
-		if(!section.classList.contains("active")){
-			section.classList.add("hide");
-		}
-	})
-})();
-
-
-window.addEventListener("load", ()=>{
-	document.querySelector(".preloader").classList.add("fadeOut");
-	setTimeout(()=>{
-		document.querySelector(".preloader").style.display="none";
-	},600)
-});
+})();*/
